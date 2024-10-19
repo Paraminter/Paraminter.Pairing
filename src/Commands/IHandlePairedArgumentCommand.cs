22110ff396ecf0a6b861db258e5699bed9a16048
ErikWe <erikwestberg7@gmail.com>
2024-10-20 00:30:43 +0200

    Added cmd for handling paired arguments

---
+++ src/Commands/IHandlePairedArgumentCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Paraminter.Pairing.Commands;
+
+using Paraminter.Arguments.Models;
+using Paraminter.Cqs;
+
+/// <summary>Represents a command to handle an argument that has been paired with a parameter.</summary>
+/// <typeparam name="TArgumentData">The type representing data about the arguments.</typeparam>
+public interface IHandlePairedArgumentCommand<out TArgumentData>
+    : ICommand
+    where TArgumentData : IArgumentData
+{
+    /// <summary>Data about the argument.</summary>
+    public abstract TArgumentData ArgumentData { get; }
+}
